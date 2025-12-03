f82b4b326fb613554c32d8b30deac6f6c52ebea1
calendar <1131821081@qq.com>
2025-12-03 15:59:57 +0800

    Quartz sync: Dec 3, 2025, 3:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Calendar's Garden",
+    pageTitle: "Calendar's Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
